372e7f134587dfd8ac3db20e1a7459d8a6b23468
luanc <luanc@DESKTOP-MLE5VU7>
2020-10-27 11:44:52 -0700

    First commit

---
+++ src/TestClass1.java
@@ -0,0 +1,10 @@
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is first git project");
+		System.out.println("for Java projects");
+	}
+
+}
